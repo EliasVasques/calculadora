d3f2209c957f898d37c2584f95ba59181b864beb
EliasVasques <eliasvasques74@gmail.com>
2022-05-23 17:03:47 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "./components/Button";
-import Visor from "./components/Visor";
+import Input from "./components/Input";
 
 import "./App.css";
 
@@ -16,29 +16,45 @@ const App = () => {
   };
 
   const handleClickOperator = (valor) => {
-    if(lastInput != '+' && lastInput != '-' && lastInput != '*' && lastInput != '/') { // não repetir operadores
-      setInput(input + valor);
+    if(!isOperator(lastInput)) { // não repetir operadores
+      setInput(input + ' ' + valor + ' ');
       setLastInput(valor);
     }
   };
 
   const handleClickDel = () => {
-
-    setInput(input.toString().slice(0, input.length - 1));
+    // setInput(input.toString().slice(0, input.length - 1));
+    const previousInput = input.split(' ');
+    previousInput.pop()
+    setInput(previousInput.join(' '))
   };
 
   const handleClickCe = () => {
     setInput("");
   };
 
+  const isOperator = (value) => {
+    if(value == '+' || value == '-' || value == '*' || value == '/') {
+      return true;
+    }
+    return false;
+  }
+
+  const inputNumbersRemovedPreviousZero = () => { // 09 -> 9 (0 antes da erro com eval)
+    const newInput = input.split(' ').map((value) => {
+        return value.replace('0', '')
+    }).join(' ');
+    return newInput;
+  }
+
   const handleClickEqual = () => {
-    setInput(eval(input));
+    setInput(String(eval(inputNumbersRemovedPreviousZero())));
   };
 
   return (
     <div className="container-calculadora">
       <div className="row">
-        <Visor valor={input}></Visor>
+        <Input valor={input}></Input>
       </div>
 
       <div className="row">
@@ -65,7 +81,8 @@ const App = () => {
         <Button onClick={handleClickOperator} className={"operator"}>/</Button>
       </div>
       <div className="row">
-        <Button onClick={handleClickNumber} className={"double-width-button"}>0</Button>
+        <Button onClick={handleClickNumber}>0</Button>
+        <Button onClick={handleClickNumber}>.</Button>
         <Button onClick={handleClickEqual} className={"double-width-button equal"}>=</Button>
       </div>
     </div>
--- src/components/Visor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Visor.css';
-
-const Visor = ( {valor} ) => {
-    return <input type="text" className="visor" readOnly value={valor} />;
-}
-
-export default Visor;
