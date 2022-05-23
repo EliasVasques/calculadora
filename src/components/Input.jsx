01172bf4a6f990d1f8ab9ae7e7215e250e4f9126
EliasVasques <81265552+EliasVasques@users.noreply.github.com>
2022-05-23 20:53:34 -0300

    Delete src/components directory

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Input.css';
-
-const Input = ( {valor} ) => {
-    return <input type="text" className="input" readOnly value={valor} />;
-}
-
-export default Input;
