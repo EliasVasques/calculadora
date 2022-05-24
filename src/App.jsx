8d06c2191c065a2bf0bb3f670108d8459c6c71a5
EliasVasques <81265552+EliasVasques@users.noreply.github.com>
2022-05-23 21:20:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   };
 
   const handleClickOperator = (valor) => {
-    if(!isOperator(lastInput)) { // não repetir operadores
+    if (!isOperator(lastInput)) { // não repetir operadores
       setInput(input + ' ' + valor + ' ');
       setLastInput(valor);
     }
@@ -34,7 +34,7 @@ const App = () => {
   };
 
   const isOperator = (value) => {
-    if(value == '+' || value == '-' || value == '*' || value == '/') {
+    if (value == '+' || value == '-' || value == '*' || value == '/') {
       return true;
     }
     return false;
@@ -42,7 +42,7 @@ const App = () => {
 
   const inputNumbersRemovedPreviousZero = () => { // 09 -> 9 (0 antes da erro com eval)
     const newInput = input.split(' ').map((value) => {
-        return value.replace('0', '')
+      return value.replace('0', '')
     }).join(' ');
     return newInput;
   }
@@ -52,38 +52,40 @@ const App = () => {
   };
 
   return (
-    <div className="container-calculadora">
-      <div className="row">
-        <Input valor={input}></Input>
-      </div>
+    <div className="container">
+      <div className="container-calculadora">
+        <div className="row">
+          <Input valor={input}></Input>
+        </div>
 
-      <div className="row">
-        <Button onClick={handleClickCe} className={"double-width-button"}>CE</Button>
-        <Button onClick={handleClickDel}>DEL</Button>
-        <Button onClick={handleClickOperator} className={"operator"}>+</Button>
-      </div>
-      <div className="row">
-        <Button onClick={handleClickNumber}>1</Button>
-        <Button onClick={handleClickNumber}>2</Button>
-        <Button onClick={handleClickNumber}>3</Button>
-        <Button onClick={handleClickOperator} className={"operator"}>-</Button>
-      </div>
-      <div className="row">
-        <Button onClick={handleClickNumber}>4</Button>
-        <Button onClick={handleClickNumber}>5</Button>
-        <Button onClick={handleClickNumber}>6</Button>
-        <Button onClick={handleClickOperator} className={"operator"}>*</Button>
-      </div>
-      <div className="row">
-        <Button onClick={handleClickNumber}>7</Button>
-        <Button onClick={handleClickNumber}>8</Button>
-        <Button onClick={handleClickNumber}>9</Button>
-        <Button onClick={handleClickOperator} className={"operator"}>/</Button>
-      </div>
-      <div className="row">
-        <Button onClick={handleClickNumber}>0</Button>
-        <Button onClick={handleClickNumber}>.</Button>
-        <Button onClick={handleClickEqual} className={"double-width-button equal"}>=</Button>
+        <div className="row">
+          <Button onClick={handleClickCe} className={"double-width-button"}>CE</Button>
+          <Button onClick={handleClickDel}>DEL</Button>
+          <Button onClick={handleClickOperator} className={"operator"}>+</Button>
+        </div>
+        <div className="row">
+          <Button onClick={handleClickNumber}>1</Button>
+          <Button onClick={handleClickNumber}>2</Button>
+          <Button onClick={handleClickNumber}>3</Button>
+          <Button onClick={handleClickOperator} className={"operator"}>-</Button>
+        </div>
+        <div className="row">
+          <Button onClick={handleClickNumber}>4</Button>
+          <Button onClick={handleClickNumber}>5</Button>
+          <Button onClick={handleClickNumber}>6</Button>
+          <Button onClick={handleClickOperator} className={"operator"}>*</Button>
+        </div>
+        <div className="row">
+          <Button onClick={handleClickNumber}>7</Button>
+          <Button onClick={handleClickNumber}>8</Button>
+          <Button onClick={handleClickNumber}>9</Button>
+          <Button onClick={handleClickOperator} className={"operator"}>/</Button>
+        </div>
+        <div className="row">
+          <Button onClick={handleClickNumber}>0</Button>
+          <Button onClick={handleClickNumber}>.</Button>
+          <Button onClick={handleClickEqual} className={"double-width-button equal"}>=</Button>
+        </div>
       </div>
     </div>
   );
